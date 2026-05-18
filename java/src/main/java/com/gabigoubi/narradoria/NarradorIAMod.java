fbbcb62078145357a4fbf89f79df8fafc299420b
Gabriel de Abreu <gabrielabreu2690@gmail.com>
2026-05-18 12:49:11 -0300

    Incluído ouvindo do jogo

--- java/src/main/java/com/gabigoubi/narradoria/NarradorIAMod.java
+++ java/src/main/java/com/gabigoubi/narradoria/NarradorIAMod.java
@@ -8,8 +8,10 @@ public class NarradorIAMod implements ModInitializer {
     public static final String MOD_ID = "narrador_ia";
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
-    @Override
+   @Override
     public void onInitialize() {
         LOGGER.info("Narrador IA - Mod inicializado com sucesso, parça!");
+        
+        GameEventListener.registerEvents();
     }
 }
