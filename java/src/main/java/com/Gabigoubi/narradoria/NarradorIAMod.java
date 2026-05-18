161ae142c3092da7c2c5f393e8ef190149184add
Gabriel de Abreu <gabrielabreu2690@gmail.com>
2026-05-18 12:09:14 -0300

    Criar java/src/main/java/com/Gabigoubi/narradoria/NarradorIAMod.java

---
+++ java/src/main/java/com/Gabigoubi/narradoria/NarradorIAMod.java
@@ -0,0 +1,15 @@
+package com.gabigoubi.narradoria;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class NarradorIAMod implements ModInitializer {
+    public static final String MOD_ID = "narrador_ia";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Narrador IA - Mod inicializado com sucesso, parça!");
+    }
+}
