07d9d3df12611bac4cab995c93c0d8deff12fc57
Gabriel de Abreu <gabrielabreu2690@gmail.com>
2026-05-19 06:26:39 -0300

    Alterada velocidade de reprodução 1.0 -> 0.85, incluído dicionário de vozes

--- server-ia/app/tts.py
+++ server-ia/app/tts.py
@@ -2,22 +2,31 @@ import io
 import scipy.io.wavfile as wavfile
 from kokoro import KPipeline
 
+
 pipeline = KPipeline(lang_code='p')
 
-def generate_speech_stream(text: str, voice_model: str) -> io.BytesIO:
+
+VOICE_MAPPING = {
+    "mulher.agressiva": "af_bella",   
+    "mulher.amigavel": "af_heart",    
+    "homem.agressivo": "am_adam",     
+    "homem.amigavel": "pm_alex"       
+}
+
+def generate_speech_stream(text: str, persona_id: str) -> io.BytesIO:
+    
+    voice_model = VOICE_MAPPING.get(persona_id, "pm_alex")
+    
     generator = pipeline(
         text, 
         voice=voice_model, 
-        speed=1.0, 
+        speed=0.85,  
         split_pattern=r'\n'
     )
     
     for _, _, audio in generator:
-        # CONVERSÃO: Transformamos o tensor do PyTorch em um array do NumPy
         audio_numpy = audio.numpy()
-        
         audio_buffer = io.BytesIO()
-        # Passamos o áudio convertido (audio_numpy) para o wavfile
         wavfile.write(audio_buffer, 24000, audio_numpy)
         audio_buffer.seek(0)
         return audio_buffer
