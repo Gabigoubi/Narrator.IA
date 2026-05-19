89a53c1ed3e2f2189c80a0caad2b6b8a5d88d96a
Gabriel de Abreu <gabrielabreu2690@gmail.com>
2026-05-19 14:42:01 -0300

    (ia): Atualizada temperatura para respostas mais criativas

--- server-ia/app/tts.py
+++ server-ia/app/tts.py
@@ -2,9 +2,7 @@ import io
 import scipy.io.wavfile as wavfile
 from kokoro import KPipeline
 
-
 pipeline = KPipeline(lang_code='p')
-
 
 VOICE_MAPPING = {
     "mulher.agressiva": "af_bella",   
@@ -14,21 +12,20 @@ VOICE_MAPPING = {
 }
 
 def generate_speech_stream(text: str, persona_id: str) -> io.BytesIO:
-    
     voice_model = VOICE_MAPPING.get(persona_id, "pm_alex")
-    
+
     generator = pipeline(
         text, 
         voice=voice_model, 
         speed=0.85,  
         split_pattern=r'\n'
     )
-    
+
     for _, _, audio in generator:
         audio_numpy = audio.numpy()
         audio_buffer = io.BytesIO()
         wavfile.write(audio_buffer, 24000, audio_numpy)
         audio_buffer.seek(0)
         return audio_buffer
-        
+
     return io.BytesIO()
