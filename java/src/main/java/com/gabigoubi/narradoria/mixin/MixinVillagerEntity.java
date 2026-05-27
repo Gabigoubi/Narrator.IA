e7ca567864ce50f442fd62eaad776e74c0c28e62
Gabriel de Abreu Oliveira <gabrielabreu2690@gmail.com>
2026-05-27 20:56:48 -0300

    feat(events): Add Edson com medo do deep dark, interação com baús, encantamento, ferraria, bigorna, ociosidade e várias coisas

--- java/src/main/java/com/gabigoubi/narradoria/mixin/MixinVillagerEntity.java
+++ java/src/main/java/com/gabigoubi/narradoria/mixin/MixinVillagerEntity.java
@@ -1,1 +1,27 @@
 
+package com.gabigoubi.narradoria.mixin;
+
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+
+@Mixin(VillagerEntity.class)
+public class MixinVillagerEntity {
+
+
+    @Inject(method = "interactMob", at = @At("HEAD"))
+    private void onInteractWithVillager(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+
+
+        if (!player.getWorld().isClient() && hand == Hand.MAIN_HAND && player instanceof ServerPlayerEntity serverPlayer) {
+
+        }
+    }
+}
